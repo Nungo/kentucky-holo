b23798232cf032a16c2392e854804c4126b6e2bd
Nungo Tshidzumba <nungotshi@gmail.com>
2026-02-19 23:20:14 +0200

    Adding final holographic changes

--- components/3d/NightScene.tsx
+++ components/3d/NightScene.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { FloatingParticles, SparkleParticles, LightBeams, FloatingTextElements, OrbitingIngredients } from './HologramEffects';
-import { HologramCharacter } from './HologramCharacter';
 import { useStoryStore } from '@/store/useStoryStore';
 
 export function NightScene() {
@@ -10,23 +9,17 @@ export function NightScene() {
 
   return (
     <>
+      {/* Ambient lighting */}
       <ambientLight intensity={0.3} />
       <pointLight position={[10, 10, 10]} intensity={0.5} color="#6366f1" />
       <pointLight position={[-10, -10, -10]} intensity={0.3} color="#ec4899" />
 
-      {/* LARGE Hologram Character (shows during story) */}
-      {isStoryActive && <HologramCharacter />}
-
-      {/* Other effects (show when no story) */}
-      {!isStoryActive && (
-        <>
-          <FloatingParticles />
-          <SparkleParticles />
-          <LightBeams />
-          <FloatingTextElements isStoryActive={false} />
-          <OrbitingIngredients />
-        </>
-      )}
+      {/* Hologram Effects */}
+      <FloatingParticles />
+      <SparkleParticles />
+      <LightBeams />
+      <FloatingTextElements isStoryActive={isStoryActive} />
+      <OrbitingIngredients />
     </>
   );
 }
